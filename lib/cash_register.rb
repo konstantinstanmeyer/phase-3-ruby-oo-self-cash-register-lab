3046dd02054eccb5262c9ed6d47879fb3ca1f56e
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-09-21 10:34:51 -0600

    commit tree 242685cb5af8d91607744c4353198ed75a2d355b

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+    attr_accessor :discount, :total, :previous_total, :items
 
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def total
+        self.total
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.previous_total = quantity * price
+        self.total += self.previous_total
+    end
+end
